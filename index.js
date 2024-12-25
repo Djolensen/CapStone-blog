52e7c7aacfa3177c3a05faa7ee0c32f11209ec80
Djolensen <ilijevskidj@gmail.com>
2024-12-25 14:58:15 +0100

    added POST functionality

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 import express from "express";
+import bodyParser from "body-parser";
 
 const app = express();
 const port = 3000;
 
+const cards = [];
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  res.render("index.ejs");
+  res.render("index.ejs", {cards});
 });
 
 app.get("/create-post", (req, res) => {
@@ -28,6 +33,14 @@ app.get("/fit3", (req, res) => {
 app.get("/fit4", (req, res) => {
   res.render("fit4.ejs");
 });
+
+app.post("/submit", (req, res) => {
+  const heading = req.body["heading"];
+  const textarea = req.body["textarea"];
+  cards.push({heading, textarea});
+  res.render("index.ejs", {cards});
+});
+
 
 
 app.listen(port, () => {
